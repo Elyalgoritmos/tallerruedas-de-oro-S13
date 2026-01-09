1ef59258d031e0dc3e004a7243ed0b437e93a2f1
Elyalgoritmos <helen.benalcazar@udla.edu.ec>
2026-01-09 11:32:10 -0500

    Add utils.h with utility function declarations

---
+++ utils.h
@@ -0,0 +1,10 @@
+#ifndef UTILS_H
+#define UTILS_H
+#include <stddef.h>
+
+int leerEntero(int *out);
+int leerFlotante(float *out);
+void leerCadena(char *buf, size_t n);
+void inicializarArchivos();
+
+#endif
